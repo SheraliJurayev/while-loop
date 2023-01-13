0eb6ac5c750fb92c4165ac3c56935c3676ba6a76
Sherali Jurayev <100849791+SheraliJurayev@users.noreply.github.com>
2023-01-13 19:29:17 +0500

    while sikli haqida

---
+++ while--lops.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 13 17:14:00 2023
+
+@author: Sh_Jurayeff
+"""
+
+    # While tsikli
+    
+#son = 1
+#while son<=5:
+#    print(son, end=" ")
+#    son=son+1
+#print("Dastur tugadi")
+
+#print("Kiritilgan sonning kvatratini dastur.")
+#savol="Istalgan sonni kiriting"
+#savol+="(Dasturdan chiqish uchun 'exit' deb yozing. ): "
+#qiymat= ' '
+#while qiymat != 'exit':
+#    qiymat=input(savol)
+#    if qiymat!='exit':
+#        print(float(qiymat)**2)
+#        
+#print("Dastur tugadi")
+    
+#print("Kiritilgan sonning kvatratini dastur.")
+#savol="Istalgan sonni kiriting"
+#savol+="(Dasturdan chiqish uchun 'exit' deb yozing. ): "
+#flag= True
+#while flag:
+#    qiymat=  input (savol)
+#    if  qiymat=='exit':
+##        flag= False
+ #   else:
+ #       print(float(qiymat)**2)
+#print("To'xtadi")
+
+       #break
+       
+#print("Kiritilgan sonning kvatratini dastur.")
+#savol="Istalgan sonni kiriting"
+##savol+="(Dasturdan chiqish uchun 'exit' deb yozing. ): "
+#while True:
+#    qiymat=input(savol)
+#    if qiymat == 'exit':
+#        break
+#    else:
+#        print(float(qiymat)**2)
+#    
+#print("To'xtadi")
+    
+
+    #continue
+    
+#son=0
+#while son<10:
+#    son+=1
+#    if son%2 !=0 :
+#        continue
+#    else:
+#        print(son)
+
+#son = 0 
+#while son<10:  #abadiy sikl
+#    son-=1
+#    if son%2!=0:
+#        continue
+#    else:
+#        print(son)
+
+kitob = "Yoqtirgan kitobingizni kiriting "
+kitob += "(dasturni to'xtatish uchun 'exit' deb yozing): "
+qiymat = ''
+while qiymat != 'exit':
+    qiymat = input(kitob)
+    if qiymat != 'exit':
+        print("Zo'r Kitob , davom eting")
+print("Dastur to'xtadi")   
+        
+    
+    
+
+
+ 
+
+
+
